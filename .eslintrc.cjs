7b5744a4dd05dc8592515f92da7700bb9dc5f276
bencmbrook <ben@transcend.io>
2023-09-02 13:01:04 -0700

    eslint-config-prettier + sort-imports change

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,12 @@
 // @ts-check
 
-/**
- * @type {import('@typescript-eslint/eslint-plugin').configs}')}
- */
 const eslintConfig = {
   root: true,
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    "prettier"
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
