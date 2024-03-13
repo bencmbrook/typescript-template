4aa73fdd951055b3b87e021137320828ff421ddd
bencmbrook <ben@transcend.io>
2024-03-13 01:23:02 -0400

    prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,7 @@
 /** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 const prettierConfig = {
   plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  singleQuote: true,
 };
 
 export default prettierConfig;
