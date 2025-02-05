9d376eda1fb46eef3761499e9d4ee53ebc83299e
bencmbrook <7354176+bencmbrook@users.noreply.github.com>
2025-02-05 14:09:41 -0500

    publint

--- scripts/scratch.ts
+++ scripts/scratch.ts
@@ -1,1 +1,7 @@
+/**
+ * Usage:
+ * pnpm script ./scripts/scratch.ts
+ *
+ * Expects `.env.local`, (run `touch .env.local` to create it)
+ */
 console.log("This is an example. Scripts aren't included in the final build.");
